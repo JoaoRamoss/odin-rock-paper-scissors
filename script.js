f5e9a44e00054bb6906e41fffe33841cd48b0a9f
João Silva <joaoramosig@gmail.com>
2022-09-06 01:13:55 +0100

    Created function to get the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+//Returns "Rock", "Paper" or "Scissors". It will act as the computer's "choice".
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    let choice = Math.floor(Math.random() * 3);
+
+    return options[choice];
+}
+
+
