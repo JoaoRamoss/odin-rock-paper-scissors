2be832718455ea68791e55af9d303a3c9caa3717
João Silva <joaoramosig@gmail.com>
2022-09-06 01:31:44 +0100

    Added the ability to play a single round

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ function getComputerChoice() {
     return options[choice];
 }
 
+//If the player's choice wins, the function returns 1, if he loses, it returns 0. It will return -1 in case of a tie.
+function whoWins(playerSel, computerSel) {
+    //Always comparing in upper case to make it case INsensitive.
+    if (playerSel.toUpperCase() === computerSel.toUpperCase())
+        return -1;
+    else if (playerSel.toUpperCase() === "ROCK" && computerSel.toUpperCase() === "SCISSORS")
+        return 1;
+    else if (playerSel.toUpperCase() === "PAPER" && computerSel.toUpperCase() === "ROCK") 
+        return 1;
+    else if (playerSel.toUpperCase() === "SCISSORS" && computerSel.toUpperCase() === "PAPER")
+        return 1;
+    else 
+        return 0;
+}
+
+//Plays a round. Receives the player selection and the computer selection.
+function playRound (playerSelection, computerSelection) {
+    const result = whoWins(playerSelection, computerSelection);
+    if (result == 1)
+        console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+    else if (result == 0)
+        console.log(`You loose! ${computerSelection} beats ${playerSelection}!`);
+    else if (result == -1) 
+        console.log("Looks like it's a tie!");
+}
 
